340829789f204a9553d888289d21c0ac470d7ea6
saugat <saugatlamichhane77@gmail.com>
2025-08-01 08:14:45 -0500

    Basic Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,15 +2,23 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="bg-stone-400 flex justify-between p-3 px-6  ">
-      <div className="flex flex-col gap-1 sm:flex-row">
-        <p>logo of company</p>
-
-        <p> home</p>
-        <p>add task</p>
+    <div className="bg-red-400 flex justify-between p-3 px-6 items-center shadow-md">
+      {/* Left side */}
+      <div className="flex flex-row w-2/3 sm:w-1/3 sm:gap-4 justify-between items-center">
+        <img src="/logo1.png" alt="Logo" className="h-14 w-auto cursor-pointer" />
+        <p className="hidden sm:block text-white font-medium hover:text-stone-200 cursor-pointer">
+          Home
+        </p>
+        <p className="hidden sm:block text-white font-medium hover:text-stone-200 cursor-pointer">
+          Add Task
+        </p>
       </div>
 
-      <div className="">login/signup</div>
+      {/* Right side */}
+      <div className="text-white font-medium">
+        <span className=" hover:text-stone-200 cursor-pointer"> Login</span> /
+        <span className=" hover:text-stone-200 cursor-pointer"> Signup </span>
+      </div>
     </div>
   );
 };
